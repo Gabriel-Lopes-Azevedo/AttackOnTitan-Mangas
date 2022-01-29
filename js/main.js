86886a9c936e90e2f153da923e9c94429c4aa7a8
Gabriel Lopes Azevedo <93387104+Gabriel-Lopes-Azevedo@users.noreply.github.com>
2022-01-29 01:17:23 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
-var button_escolher = document.getElementById('button_escolher');
+let button_escolher = document.getElementById('button_escolher');
 button_escolher.addEventListener('click', function addClass (){
   let button_escolher = document.getElementById('button_escolher');
   let aotm_principal = document.getElementById('aotm_principal');
   let volumes = document.getElementById('volumes');
-  let container_geral = document.getElementById('container_geral')
-  button_escolher.classList.add("outsidePage");
-  aotm_principal.classList.add("outsidePage");
-  volumes.classList.remove("volumes-outPage");
+  let voltar = document.getElementById('Voltar');
+  let titulo_volume = document.getElementById('titulo_volume');
+  button_escolher.classList.add("outPage");
+  aotm_principal.classList.add("outPage");
+  volumes.classList.remove("outPage");
   volumes.classList.add("inPage");
-  container_geral.classList.remove("overflow-hidden");
+  voltar.classList.remove("outPage");
+  voltar.classList.add("inPage");
+  titulo_volume.classList.remove("outPage");
+  titulo_volume.classList.add("inPage");
+});
+
+let button_voltar = document.getElementById('Voltar');
+button_voltar.addEventListener('click', function voltarMenu (){
+  let volumes = document.getElementById('volumes');
+  let voltar = document.getElementById('Voltar');
+  let button_escolher = document.getElementById('button_escolher');
+  let aotm_principal = document.getElementById('aotm_principal');
+  let titulo_volume = document.getElementById('titulo_volume');
+  volumes.classList.add("outPage");
+  voltar.classList.add("outPage");
+  button_escolher.classList.remove("outPage")
+  aotm_principal.classList.remove("outPage");
+  aotm_principal.classList.add("inPage");
+  button_escolher.classList.add("inPage");
+  titulo_volume.classList.remove("inPage")
+  titulo_volume.classList.add("outPage");
 });
 
